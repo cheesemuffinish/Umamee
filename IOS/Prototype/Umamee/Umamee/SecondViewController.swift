7f35b17d6202ce569c818651da46b13732bbf5e7
DerekAThompson <derekt123@outlook.com>
2019-10-06 16:38:14 -0400

    IOS App Connection with Firebase
    Installed Cocoapods and setup Firebase connection within IOS application.
    Connection confirmed from Firebase servers with Umamee application.

---
+++ IOS/Prototype/Umamee/Umamee/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Umamee
+//
+//  Created by Derek Thompson on 10/6/19.
+//  Copyright © 2019 Derek Thompson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
